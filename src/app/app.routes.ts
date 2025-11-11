d7395f897498edcde88ee853eb01f32939ad7ba2
Ruser <user@DESKTOP-NS5BTI0>
2025-11-11 15:18:48 +0100

    c'est la première version de mon site angular à trois page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Accueil } from './pages/accueil/accueil';
+import { APropos } from './pages/a-propos/a-propos';
+import { NousContacter } from './pages/nous-contacter/nous-contacter';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Accueil, title: 'Accueil' },
+  { path: 'a-propos', component: APropos, title: 'À propos' },
+  { path: 'nous-contacter', component: NousContacter, title: 'Nous contacter' }
+];
